311cc7705bbc094d9a4055f20825018c82fa93a2
Abandoned Cart <twistedumbrella@gmail.com>
2021-09-29 08:44:24 -0400

    Evaluate registered configChanges overrides
    https://developer.android.com/guide/topics/manifest/activity-element#config

--- app/src/main/java/com/samsung/android/app/shealth/tracker/pedometer/service/coverwidget/StepCoverAppWidget.kt
+++ app/src/main/java/com/samsung/android/app/shealth/tracker/pedometer/service/coverwidget/StepCoverAppWidget.kt
@@ -62,7 +62,6 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
 import android.widget.RemoteViews
 import com.sec.android.app.shealth.*
 
@@ -163,5 +162,14 @@ class StepCoverAppWidget: AppWidgetProvider() {
 
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
+    }
+
+    override fun onAppWidgetOptionsChanged(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetId: Int,
+        newOptions: Bundle?
+    ) {
+        // TODO: Attempt to override configuration changes
     }
 }
