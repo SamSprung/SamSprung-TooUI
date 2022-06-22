68b283c08abccda0d6ff795207dffd2485dd08e3
Abandoned Cart <twistedumbrella@gmail.com>
2022-06-22 00:06:24 -0400

    Strip orientation setting before view removal

--- app/src/main/java/com/eightbit/samsprung/launcher/OrientationManager.kt
+++ app/src/main/java/com/eightbit/samsprung/launcher/OrientationManager.kt
@@ -1,6 +1,7 @@
 package com.eightbit.samsprung.launcher
 
 import android.content.Context
+import android.content.pm.ActivityInfo
 import android.graphics.PixelFormat
 import android.view.View
 import android.view.WindowManager
@@ -12,18 +13,19 @@ class OrientationManager(context: Context) {
     private val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
     private val orientationManager = LinearLayout(context)
 
+    private val orientationLayout = WindowManager.LayoutParams(
+        WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+        PixelFormat.TRANSPARENT
+    )
+
     companion object {
         private var orientationView: SoftReference<LinearLayout>? = null
     }
 
     fun addOrientationLayout(orientation: Int) {
-        val orientationLayout = WindowManager.LayoutParams(
-            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-            WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
-                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
-                    WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
-            PixelFormat.TRANSPARENT
-        )
         orientationLayout.screenOrientation = orientation
         windowManager.addView(orientationManager, orientationLayout)
         orientationManager.visibility = View.VISIBLE
@@ -33,8 +35,12 @@ class OrientationManager(context: Context) {
     fun removeOrientationLayout() {
         try {
             if (null != orientationView) {
+                orientationLayout.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+                windowManager.updateViewLayout(orientationView!!.get(), orientationLayout)
                 windowManager.removeViewImmediate(orientationView!!.get())
             } else {
+                orientationLayout.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+                windowManager.updateViewLayout(orientationManager, orientationLayout)
                 windowManager.removeViewImmediate(orientationManager)
             }
         } catch (ignored: Exception) { }
